04a1ecf90962439b64ea3e9407bcf9b468586fd7
Nitin <nparcha19@gmail.com>
2022-08-18 09:43:52 +0530

    second commit

--- toasty/src/main/java/com/skorpiontech/toasty/Toaster.java
+++ toasty/src/main/java/com/skorpiontech/toasty/Toaster.java
@@ -4,7 +4,7 @@ import android.content.Context;
 import android.widget.Toast;
 
 public class Toaster {
-    public static void simpleToast(Context context, String msg){
+    public static void makesimpleToast(Context context, String msg){
         Toast.makeText(context, msg, Toast.LENGTH_SHORT).show();
     }
 }
